2bd43ff38728f352193a0c7c1d0aafa4bddd2063
Jingyu <jingyuhe@hotmail.com>
2017-03-03 11:03:34 -0600

    update starterScript for question 2

--- hw04/starterScript.R
+++ hw04/starterScript.R
@@ -1,22 +1,54 @@
-# the following line will install and update 
-# packages that we are using for the ML course
-# NOTE: this may take some time
-# NOTE: ignore potential warnings, but not errors
-source("https://raw.githubusercontent.com/ChicagoBoothML/HelpR/master/booth.ml.packages.R")
+PackageList=c("MASS", 
+              "ISLR",
+              "animation",
+              "ElemStatLearn",
+              "glmnet",
+              "textir",
+              "nnet",
+              "methods",
+              "statmod",
+              "stats",
+              "graphics",
+              "RCurl",
+              "jsonlite",
+              "tools",
+              "utils",
+              "data.table",
+              "gbm",
+              "ggplot2",
+              "randomForest",
+              "tree",
+              "class",
+              "kknn",
+              "e1071",
+              "data.table",
+              "R.utils",
+              "recommenderlab")
+NewPackages=PackageList[!(PackageList %in% 
+                            installed.packages()[,"Package"])]
+if(length(NewPackages)) install.packages(NewPackages)
+lapply(PackageList,require,character.only=TRUE)#array function
 
-# this block of code will read in all the data
-library(jsonlite)
-# use this line if you have downloaded "videoGames.json.gz" to your current folder
+if (!("h20" %in% rownames(installed.packages()))) { 
+  # Now we download, install and initialize the H2O package for R.
+    install.packages("h2o", 
+                     type="source", 
+                     repos=(c("http://h2o-release.s3.amazonaws.com/h2o/master/3232/R")))
+}
+
+
+if(!file.exists("videoGames.json.gz")){
+  download.file( "https://github.com/ChicagoBoothML/MachineLearning_Fall2015/raw/master/Programming%20Scripts/Lecture07/hw/videoGames.json.gz",destfile="videoGames.json.gz")
+}
 fileConnection <- gzcon(file("videoGames.json.gz", "rb"))
-# use this line if the file is not downloaded to your computer (say working on rstudio.chicagobooth.edu)
-# fileConnection <- gzcon(url("https://github.com/ChicagoBoothML/ML2016/raw/master/hw04/videoGames.json.gz"))
-data = stream_in(fileConnection)
+InputData = stream_in(fileConnection)
 
-library("recommenderlab")
-# create a ratingData matrix using reviewerID, itemID, and rating
-ratingData = as(data[c("reviewerID", "itemID", "rating")], "realRatingMatrix")
+
+ratingData = as(InputData[c("reviewerID", "itemID", "rating")], "realRatingMatrix")
 # we keep users that have rated more than 2 video games
+
 ratingData = ratingData[rowCounts(ratingData) > 2,]
+
 # we will focus only on popular video games that have 
 # been rated by more than 3 times
 ratingData = ratingData[,colCounts(ratingData) > 3]
@@ -24,14 +56,13 @@ ratingData = ratingData[,colCounts(ratingData) > 3]
 dim(ratingData)
 
 # example on how to recommend using Popular method
-r = Recommender(ratingData, method="Popular")
+r = Recommender(ratingData, method="POPULAR")
 
-# recommend 5 items to user it row 10
-rec = predict(r, ratingData[10, ], type="topNList", n=5)
+# recommend 5 items to user it row 13
+rec = predict(r, ratingData[13, ], type="topNList", n=5)
 as(rec, "list")
 
 # predict ratings 
-rec = predict(r, ratingData[10, ], type="ratings")
+rec = predict(r, ratingData[13, ], type="ratings")
 as(rec, "matrix")
-
 
